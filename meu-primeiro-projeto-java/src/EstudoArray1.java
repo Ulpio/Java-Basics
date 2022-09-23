aa2921f63f59834aff6aa656a93728a108908819
Ulpio <ulpionetto0@gmail.com>
2022-09-22 22:15:49 -0300

    estudos

---
+++ meu-primeiro-projeto-java/src/EstudoArray1.java
@@ -0,0 +1,17 @@
+
+
+public class EstudoArray1 {
+
+    public static void main(String[] args) {
+    
+    
+    int[] idades = new int[10];
+
+    for( int i =0; i<10;i++){
+        idades[i] = i*10;
+    }
+    for (int i =0;1<10;i++){
+        System.out.println(idades[i]);
+    }
+    }
+}
